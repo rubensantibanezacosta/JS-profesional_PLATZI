b9e5786b2cadba1ee8231548137e494b26bf57e8
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-08-15 22:16:41 +0100

    configurar Service Worker, cambiar live-server a parcel-bundler e iniciacion con TypeScript

--- assets/index.js
+++ assets/index.js
@@ -15,3 +15,9 @@ buttonPlay.onclick = () => {
 buttonMute.onclick = () => {
     player.toggleMute();
 };
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./sw.js').catch(error => {
+        console.log(error.message);
+    });
+}
