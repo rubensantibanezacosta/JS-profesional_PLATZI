52a33cfe156e98c00f5c64c321adc4a1a78c46e2
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-08-16 22:21:41 +0100

    implementar Ads con singleton en el video

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,14 @@
 import  MediaPlayer from "./mediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause";
+import Ads from "./plugins/Ads";
 
 
 const video=document.querySelector("video");
 const buttonPlay: HTMLElement = document.querySelector("#button__play");
 const buttonMute: HTMLElement = document.querySelector("#button__mute");
 const player=new MediaPlayer({el: video, plugins:[
-    new AutoPlay(), new AutoPause()
+    new AutoPlay(), new AutoPause(), new Ads()
 ]});
 buttonPlay.onclick = () => {
     player.togglePlay();
